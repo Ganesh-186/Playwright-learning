7ff0ba6632a6a9401735d73028fe2b6518d52fc3
Ganesh-186 <ganeshravichandran.18@gmail.com>
2026-07-06 17:31:52 +0530

    Revert "revert"
    This reverts commit 915c2a085a27a61fad57ccb0375a852911b66dbb.

--- tests/Record.spec.js
+++ tests/Record.spec.js
@@ -8,10 +8,10 @@ test('Screeen Record',async()=>{
             dir:'./record'
         }
     });
-    // const page1 = await context.newPage();
-    // const page2 = await context.newPage();
-    // await page1.goto('https://www.flipkart.com')
-    // await page2.goto('https://www.amazon.in')
+    const page1 = await context.newPage();
+    const page2 = await context.newPage();
+    await page1.goto('https://www.flipkart.com')
+    await page2.goto('https://www.amazon.in')
 
     name="Sai"
     age = 39
